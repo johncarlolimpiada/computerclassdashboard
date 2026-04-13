ba7c5cc278d76fffc649aa882b8230e43fc48d4e
scratchmasterj21 <john.limpiada@felice.ed.jp>
2026-04-13 12:54:55 +0900

    adfa

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -37,5 +37,5 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(getURL())
+  return NextResponse.redirect(`${origin}/`)
 }
